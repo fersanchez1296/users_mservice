aa43b99d0c9c2087475872ff07d82889aff1d281
Fernando <fernando.sanchezplascencia@jalisco.gob.mx>
2025-01-20 16:04:17 -0600

    se agrego una nueva variable de entorno

--- src/db/db_connection.js
+++ src/db/db_connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/tickets?authSource=admin&directConnection=true`
+      `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}?authSource=admin&directConnection=true`
     );
     console.log(">>>DB is connected!");
   } catch (err) {
