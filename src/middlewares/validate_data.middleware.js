ba0aca8f9b65c8acec265a296df8372f333acf26
fergob <fernando.sanchezplascencia@jalisco.gob.mx>
2025-01-29 08:25:52 -0600

    se eliminaron propiedades del body

--- src/middlewares/validate_data.middleware.js
+++ src/middlewares/validate_data.middleware.js
@@ -7,9 +7,14 @@ export const validateData = (req, res, next) => {
   delete req.body.Password;
   delete req.body.__v;
   delete req.body.id;
+  delete req.body.areaUsuario;
+  delete req.body.usernameUsuario;
+  delete req.body.correoUsuario;
+  delete req.body.direcciongeneralUsuario;
   console.log("Datos en validate data", req.body);
   const { error } = usuariosSchema.validate(req.body);
   if (error) {
+    console.log(error);
     return res
       .status(400)
       .json({ error: error.details?.[0]?.message || "Error de validacion" });
